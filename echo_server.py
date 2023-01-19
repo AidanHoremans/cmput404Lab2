422e68877fe6c98ba065ef167c3b873b77b1f72b
Aidan Horemans <aidanjhoremans@gmail.com>
2023-01-18 20:39:13 -0700

    finished most questions

--- echo_server.py
+++ echo_server.py
@@ -1,4 +1,5 @@
-import socket, requests
+import socket
+from threading import Thread
 
 BYTES = 4096
 HOST = "127.0.0.1"
@@ -13,18 +14,27 @@ def handle_connection(conn, addr):
                 break
             print(data)
             conn.send(data)
-        
-
 
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #SOL_SOCKET = socket level, and we reuse the address -> how do we reuse a port? tells the operating system to allow us to use this even if the socket is in timeout
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         
         s.listen()
 
         conn, addr = s.accept()
         handle_connection(conn, addr)
     return
+
+def start_threaded_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSock:
+        serverSock.bind((HOST, PORT))
+        serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSock.listen() 
+
+        while True:
+            conn, addr = serverSock.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
 
 start_server()
